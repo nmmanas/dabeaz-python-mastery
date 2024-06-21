b7e7eab9400329de6fe822a8b12c711dba9cdeeb
Manas <nmmanas@gmail.com>
2024-06-21 23:08:39 +0530

    solution 1-6

---
+++ MySolutions/1_6/pcost.py
@@ -0,0 +1,19 @@
+def portfolio_cost(filename):
+    total_price = 0
+    with open(filename) as file:
+
+        for line in file:
+                data = line.split()
+                try:
+                    qty = int(data[1])
+                    price = float(data[2])
+                    total_price += qty*price
+                except ValueError as e:
+                    print("Couldn't parse:", repr(line))
+                    print("Reason:", e)
+    return total_price
+
+if __name__ == "__main__":
+    file_name = '../../Data/portfolio.dat'
+    total_price = portfolio_cost(file_name)
+    print(total_price)
